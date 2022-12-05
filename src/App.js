b6ed29d011d3112d6dd31894f5939bf12f437079
arvidwedtstein <bassgamer03@gmail.com>
2022-12-05 12:37:32 +0100

    GSAP init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+   
+function Box({ children }) {
+  return <div className="box">{children}</div>;
+}
 
-function App() {
+function Circle({ children }) {
+  return <div className="circle">{children}</div>;
+}
+
+export default function App() {
+  const [reversed, setReversed] = useState(false);
+  const app = useRef();
+  // store the timeline in a ref.
+  const tl = useRef();
+      
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      // add a box and circle animation to our timeline and play on first render
+    console.log("creating timeline");
+    tl.current && tl.current.progress(0).kill();
+    tl.current = gsap.timeline()
+      .to(".box", {
+        rotation: 360
+      })
+      .to(".circle", {
+        x: 100
+      });
+    }, app);
+    return () => ctx.revert();
+  }, []);
+  
+  useEffect(() => {
+    // toggle the direction of our timeline
+    console.log("toggling reverse to", reversed);
+    tl.current.reversed(reversed);    
+  }, [reversed]);
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" ref={app}>
+      <div>
+        <button onClick={() => setReversed(!reversed)}>Toggle</button>
+      </div>
+      <Box>box</Box>
+      <Circle>circle</Circle>
     </div>
   );
 }
-
-export default App;
